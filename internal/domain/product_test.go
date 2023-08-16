4ab66baeaa9059ba4e04be3ae7f1c3b60e31aa5e
Bruno Lino <brunolino2026@gmail.com>
2023-08-15 22:03:13 -0300

    refactor: Refactor product test

--- internal/domain/product_test.go
+++ internal/domain/product_test.go
@@ -7,16 +7,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateNewProductWithNameDescriptionPrice(t *testing.T) {
-	expectedName := "test"
-	expectedDescription := "test"
-	expectedPrice := 45.67
+func TestProduct(t *testing.T) {
+	t.Run("Should Create new Product when Name, Description and Price are passed", func(t *testing.T) {
+		expectedName := "test"
+		expectedDescription := "test"
+		expectedPrice := 45.67
 
-	product := domain.NewProduct(expectedName, expectedDescription, expectedPrice)
+		product := domain.NewProduct(expectedName, expectedDescription, expectedPrice)
 
-	assert.NotEmpty(t, product.ID)
-	assert.Equal(t, expectedName, product.Name)
-	assert.Equal(t, expectedDescription, product.Description)
-	assert.Equal(t, expectedPrice, product.Price)
-	assert.True(t, product.Active)
+		assert.NotEmpty(t, product.ID)
+		assert.Equal(t, expectedName, product.Name)
+		assert.Equal(t, expectedDescription, product.Description)
+		assert.Equal(t, expectedPrice, product.Price)
+		assert.True(t, product.Active)
+	})
 }
